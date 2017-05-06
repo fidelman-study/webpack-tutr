e3fcfeffc508e0d3aa56fbe39aed23fe62085ddf
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-05-06 18:19:38 +0200

    NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
 'use strict';
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
 module.exports = {
     entry: "./home",
     output: {
         filename: "build.js",
         library: "home"
     },
-    watch: true,
+    watch: NODE_ENV == 'development',
     watchOptions: { // to change timeout from IDE to save evth
         aggregateTimeout: 100
     },
-    devtool: 'source-map'
+    devtool: NODE_ENV == 'development' ? 'source-map' : false,
+    plugins: [ // needs local webpack, pass vars to app
+        //new webpack.EnvironmentPlugin('NODE_ENV') // always set NODE_ENV
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV) // will be NODE_ENV in app, always JSON.stringify
+        }) // not always set NODE_ENV
+    ]
 };
